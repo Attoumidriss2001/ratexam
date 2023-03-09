da1ce5367dacce96fbd5768e0d04026802093c7f
HP <attoumi2001@gmail.com>
2023-03-09 20:10:08 +0100

    EXAMANGULAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './component/photo-list/photo-list.component';
+import { PhotoSingleComponent } from './component/photo-single/photo-single.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoListComponent,
+    PhotoSingleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
